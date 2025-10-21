5dedb42cd9eb82837cacd05aed65214ad67d215c
vivekratr <vivekratr@gmail.com>
2025-10-22 00:06:34 +0530

    `Updated database connection URL in database.js, added new build and vercel-build scripts in package.json, and updated terms in database/schema.sql`

--- backend/config/database.js
+++ backend/config/database.js
@@ -2,7 +2,7 @@ import { Pool } from 'pg';
 import 'dotenv/config';
 
 const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
+    connectionString: process.env.DATABASE_URL1,
     ssl: process.env.NODE_ENV === 'production' ? { rejectUnauthorized: false } : false,
     max: 20,
     idleTimeoutMillis: 30000,
